5ff664953261302a9c3837c6cb903049e6f40989
Nathan Brixius <nbrixius@ebay.com>
2021-01-08 21:54:56 -0800

    Adding sentiment analysis

--- proust_names.py
+++ proust_names.py
@@ -1,4 +1,4 @@
-# proust_names.py / Dec 2020 / Nathan Brixius @natebrix
+# proust_names.py / Jan 2021 / Nathan Brixius @natebrix
 #
 # Counts occurrences of proper names in «À la recherche du temps perdu» by
 # Marcel Proust.
@@ -13,16 +13,19 @@
 # I am not an expert in NLP / spacy, so buyer beware.
 #
 # *** packages you'll need:
-#   bs4, pandas, spacy, matplotlib
+#   bs4, pandas, spacy, matplotlib, spacytextblob
 # also, you will need the largest french-language spacy model:
 # python -m spacy download fr_core_news_lg
 #
 #
 # *** how to run me:
-# 1) one time only: time: write_proust_pages()
-# 2) islt, et = get_proust_names()
-# 3) rc = get_ref_count_by_chapter(et, ['Albertine', 'Charlus'])
+# 1) islt, et = get_proust_names()
+# 2) names = top_entities(et, 5).index
+# 3) rc = get_ref_count_by_chapter(et, names)
 # 4) name_frequency_plot(rc, volume_starts)
+
+# moving average of sentiment over time
+# plot sentiment as color bar
 
 from bs4 import BeautifulSoup
 from collections import Counter
@@ -30,6 +33,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
 from spacy.pipeline import merge_entities, merge_noun_chunks
 from urllib.request import urlopen
 
@@ -106,22 +110,29 @@ def canonicalize_entity(e, i, ents):
     else:
         return e.text
 
-    
+# Get a list of all entity names in the given text.    
 def entities(text):
     ents = [e for e in text.ents if is_entity_pos(e)]
     return [canonicalize_entity(e, i, ents) for i, e in enumerate(ents)]
 
 
+def words_in_list(text, words):
+    return [e.text.lower() for i, e in enumerate(text) if e.text.lower() in words]
+
+
 def load_spacy(model='fr_core_news_lg'):
     print(f'loading spacy model {model}')
+    spacy_text_blob = SpacyTextBlob()
     nlp = spacy.load(model)
     nlp.add_pipe(proust_sentence_start, before='parser')
     nlp.add_pipe(proust_proper_name, before='parser')
     nlp.add_pipe(merge_entities) # ['Saint', 'Loup'] -> ['Saint Loup']
+    nlp.add_pipe(spacy_text_blob) # for sentiment
     return nlp
 
 nlp = load_spacy()
 
+# get a single HTML page of ISLT, either from 'file' or 'web'.
 def get_proust_page(id, source='file'):
     if source == 'file':
         file = f'data/fr/islt_fr_{id:03}.html'
@@ -134,7 +145,8 @@ def get_proust_page(id, source='file'):
     else:
         raise ValueError(f'Invalid source "{source}". Valid sources = file, web.')
 
-    
+
+# write the contents of ISLT pages to files.
 def write_proust_pages(start=1, end=486, source='web'):
     for id in range(start, end+1):
         page = get_proust_page(id, source)
@@ -199,10 +211,22 @@ def entity_table(chapters):
     #names['name'] = names['name_pure'].apply(lambda n: aliases[n] if n in aliases else n)
     return names
 
+def word_freq_table(chapters, words):
+    print(f'Finding word frequency within {len(chapters)} chapters.')
+    rows = []
+    for index_chapter, chapter in enumerate(chapters):
+        for index_para, para in enumerate(chapter):
+            rows.extend([[index_chapter, index_para, e] for e in words_in_list(nlp(para), words)])
+    names = pd.DataFrame(rows, columns=['chapter', 'paragraph', 'name'])
+    return names
 
 # get entity counts
 def entity_count(et):
-    return et.groupby('name').count()
+    return et.groupby('name').count().drop('paragraph',axis=1).rename(columns={'chapter':'count'})
+
+
+def top_entities(et, count):
+    return entity_count(et).sort_values('count', ascending=False).head(count)
 
 
 # run write_proust_pages() before you run me (to download the text)
@@ -212,26 +236,46 @@ def get_proust_names():
     return islt, et
 
 
+def get_references_for_names(et, names):
+    keep = pd.DataFrame(names, columns=['name'])
+    return et.join(keep.set_index('name'), on='name', how='inner')
+
+
 # Get the number of references to each name by chapter, given an entity table
 def get_ref_count_by_chapter(et, names):
-    keep = pd.DataFrame(names, columns=['name'])
-    et2 = et.join(keep.set_index('name'), on='name', how='inner')
-    et3 = et2.groupby(['name', 'chapter']).count().reset_index()
-    return pd.pivot_table(et3, values='name_core', index=['chapter'], columns=['name']).reset_index().fillna(0)
+    et2 = get_references_for_names(et, names)
+    et3 = et2.groupby(['name', 'chapter']).count().reset_index().rename({'paragraph':'count'}, axis=1)
+    return pd.pivot_table(et3, index='chapter', columns='name', values='count').reset_index().fillna(0)
 
 
-# the first chapter of each of the seven volumes
-volume_starts = [0, 102, 182, 265, 334, 390, 432]
+# smooth a ref_count dataframe for visualization
+def smooth_ref_count(rc, window=3):
+    rcs = rc.rolling(window).mean().fillna(rc) 
+    rcs['chapter'] = rc['chapter']
+    return rcs
 
+
+# the first chapter of each of the seven volumes, plus a sentinel
+volume_starts = [0, 102, 182, 265, 334, 390, 432, 486]
+short_labels = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+
+
+def volume_column(df, chapter='chapter', volume='volume'):
+    return df[chapter].apply(lambda c: bisect.bisect_left(volume_starts, c+1))
+
+    
 # Create a plot of frequency of character references by chapter
 # df is assumed to be a data frame whose first column is chapter
 # and the remaining columns are reference counts for different characters.
 # You can get such a table by calling get_ref_count_by_chapter
-def name_frequency_plot(df, starts):
+#
+# smoothing a bit looks nice. use smooth_ref_count for that.
+def name_frequency_plot(df, labels=short_labels, starts=volume_starts[:7], transform=None, color_map='Blues', norm=None):
     xy = np.array(df)
-    xy[:, 1:] = np.sqrt(xy[:, 1:]) # looks better...
+    if transform:
+        xy[:, 1:] = transform(xy[:, 1:]) 
     row_count = xy.shape[1] - 1
-    plt.rcParams["figure.figsize"] = 8,5
+    plt.rcParams["figure.figsize"] = 8,row_count
 
     # thanks stackoverflow:
     #  https://stackoverflow.com/questions/45841786/creating-a-1d-heat-map-from-a-line-graph
@@ -242,20 +286,21 @@ def name_frequency_plot(df, starts):
     for ax_i, ax in enumerate(axs):
         i = ax_i + 1
         y = xy[:, i]
-        ax.imshow(y[np.newaxis,:], cmap="YlGnBu", aspect="auto", extent=extent)
+        ax.imshow(y[np.newaxis,:], cmap=color_map, aspect="auto", extent=extent, norm=norm)
         ax.set_yticks([])
         ax.set_ylabel(df.columns[i])
         ax.set_xlim(extent[0], extent[1])
         ax.set_xticks(starts)
-        ax.set_xticklabels(['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
+        ax.set_xticklabels(labels)
     plt.tight_layout()
     plt.show()
 
+    
 # flatten the chapters of ISLT into a single string.
 def flatten_islt(islt):
     print('Flattening ISLT chapters.')
     all_c = [" \n".join(c) for c in islt]
-    all_islt = " ".join([c for c in all_c])
+    all_islt = "\n".join([c for c in all_c])
     return all_islt
 
 
@@ -294,3 +339,54 @@ def summary_stats(doc):
 
     df = pd.DataFrame(stats)
     return df, word_freq, word_freq_no_stop, lemma_freq
+
+
+# uses spacytextblob to get sentiment information for each paragraph
+# This takes awhile to run...
+def get_sentiment(islt):
+    print(f'getting sentiment for {len(islt)} chapters, please wait...')
+    ss = [[(len(para), nlp(para)._.sentiment) for para in chap] for chap in islt]
+    print(f'extracting polarity and subjectivity')
+    ss2 = [(c, p, s[1].polarity, s[1].subjectivity, len(s[1].assessments), s[0]) for c, ss_c in enumerate(ss) for p, s in enumerate(ss_c)]
+    return pd.DataFrame(ss2, columns=['chapter', 'paragraph', 'polarity', 'subjectivity', 'assessed', 'length'])
+
+
+# joins entity and sentiment tables
+def join_sentiment(et, sentiment):
+    return et.join(sentiment.set_index(['chapter', 'paragraph']), on=['chapter', 'paragraph'])
+
+
+# aggregates per-chapter sentiment by the specified key(s). The chapter count is given as the
+# 'count' column
+def agg_sentiment(ets, group):
+    return ets.groupby(group).agg({'chapter': 'count', 'polarity': 'mean', 'subjectivity':'mean'}).rename(columns={'chapter':'count'}).reset_index()
+
+
+# not all text has tokens with identified sentiment. Here's how to filter those out:
+# sentiment.query('assessed!=0')
+
+
+# get per-chapter sentiment by name
+def get_sentiment_by_name(et, sentiment):
+    ets = join_sentiment(et, sentiment)
+    return agg_sentiment(ets, 'name').sort_values('count', ascending=False)
+
+
+# get sentiment by volume
+def get_sentiment_by_volume(et, sentiment):
+    # restrict et to names
+    ets = join_sentiment(et, sentiment)
+    return agg_sentiment(ets, 'volume').sort_values('volume')
+
+# get sentiment by name-volume pair
+def get_sentiment_by_name_volume(et, sentiment, names):
+    # restrict et to names
+    et_name = get_references_for_names(et, names)
+    ets = join_sentiment(et_name, sentiment)
+    return agg_sentiment(ets, ['name', 'volume']).sort_values(['name', 'volume'])
+
+
+def get_polarity_rank(s_by_n, min_count=100):
+    s_top = s_by_n.query(f'count >= {min_count}')
+    return s_top.polarity.rank(pct=True).sort_values()
+
